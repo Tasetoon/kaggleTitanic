03c175e425149d8d1531758bac04bbf49f1485f7
Philip <fsarg2007@gmail.com>
2026-05-09 02:11:19 +0300

    --finaled DecisionTree model, --update preprocessing.py, .gitignore

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def prepare_train(path: str):
+def prepare_data(path: str):
     train = pd.read_csv(path, on_bad_lines='skip', na_values='?')
 
     train = train.drop(columns=['PassengerId'])
@@ -12,11 +12,7 @@ def prepare_train(path: str):
     train['Age'] = train['Age'].fillna(train['Age'].mean())
     train['Relatives'] = train['SibSp'] + train['Parch']
 
-    
-
-
     sex_categorical_values = {'female': 0, 'male': 1}
-    embarked_categorical_values = {'S': 0, 'Q':1, 'C':2}
     train['Sex'] = train['Sex'].apply(lambda x: sex_categorical_values[x])
     train = pd.get_dummies(train, columns=['Embarked'], drop_first=True, dtype=int)
 
