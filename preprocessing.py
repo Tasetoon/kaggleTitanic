b7e20bd8dabff204fbe17af11623f868ab2a798a
Philip <fsarg2007@gmail.com>
2026-05-09 14:05:01 +0300

    refactore

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,14 @@
 import pandas as pd
+import numpy as np
 
-def prepare_train(path: str):
+def prepare_data(path: str):
     train = pd.read_csv(path, on_bad_lines='skip', na_values='?')
+
+    train['Title'] = train['Name'].str.extract(r' ([A-Za-z]+)\.')
+    train['Title'] = train['Title'].replace(['Lady','Countess','Capt','Col','Don','Dr','Major','Rev','Sir','Jonkheer','Dona'], 'Rare')
+    train['Title'] = train['Title'].replace('Mlle', 'Miss')
+    train['Title'] = train['Title'].replace('Ms', 'Miss')
+    train['Title'] = train['Title'].replace('Mme', 'Mrs')
 
     train = train.drop(columns=['PassengerId'])
     train = train.drop(columns=['Name'])
@@ -11,11 +18,12 @@ def prepare_train(path: str):
     train['Embarked'] = train['Embarked'].fillna(train['Embarked'].mode()[0])
     train['Age'] = train['Age'].fillna(train['Age'].mean())
     train['Relatives'] = train['SibSp'] + train['Parch']
-
+    train['IsChild'] = (train['Age'] < 12).astype(int)
+    train['FareLog'] = np.log1p(train['Fare'])
+    train['IsAlone'] = (train['Relatives'] == 0).astype(int)
 
     sex_categorical_values = {'female': 0, 'male': 1}
-    embarked_categorical_values = {'S': 0, 'Q':1, 'C':2}
     train['Sex'] = train['Sex'].apply(lambda x: sex_categorical_values[x])
-    train['Embarked'] = train['Embarked'].apply(lambda x: embarked_categorical_values[x])
+    train = pd.get_dummies(train, columns=['Embarked', 'Title'], drop_first=True, dtype=int)
 
     return train
